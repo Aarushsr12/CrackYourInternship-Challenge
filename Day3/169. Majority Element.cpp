196bda8676286bebf1d6ed9dea0388f8dd23af87
Aarush Srivastava <20BCS4110@cuchd.in>
2022-05-05 23:20:37 +0530

    Create 169. Majority Element.cpp

---
+++ Day3/169. Majority Element.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        //Moore-Voting Algorithm
+        int n = nums.size();
+        int majority = nums[0];
+        int count = 1;
+        for(int i=1; i<nums.size(); ++i){
+            if(nums[i] == majority){
+                count++;
+            }
+            else{
+                count--;
+            }
+            
+            if(count == 0){
+               majority = nums[i];
+                count = 1;
+            }
+        }
+        count = 0;
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] == majority){
+                count++;
+            }
+        }
+        if(count > (n/2)){
+            return majority;
+        }
+        else{
+            return -1;
+        }
+    }
+};
