1a4def16e46d53be0f2f3d757cf23c0f76b018a6
Aarush Srivastava <20BCS4110@cuchd.in>
2022-05-05 23:18:32 +0530

    Create 560. Subarray Sum Equals K.cpp

---
+++ Day3/560. Subarray Sum Equals K.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int curr_sum = 0;
+        int count = 0;
+        unordered_map<int,int>m;
+        for(int i=0; i<nums.size(); ++i){
+            curr_sum += nums[i];
+            if(curr_sum == k){
+                count++;
+            }
+            if(m.find(curr_sum -k) != m.end()){
+                count += m[curr_sum -k];
+            }
+            m[curr_sum]++;
+        }
+        return count;
+    }
+};
